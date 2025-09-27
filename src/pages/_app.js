e840a95941fb02e20c4488f6a9f3ed262cc7f912
Pranav Aggarwal <pranavaggarwal1100@gmail.com>
2025-09-27 13:01:31 +0530

    build fixes 3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -37,7 +37,7 @@ function ProtectedRoute({ children }) {
       <div className="min-h-screen flex items-center justify-center">
         <div className="text-center">
           <h1 className="text-2xl font-bold text-gray-900 mb-2">Access Denied</h1>
-          <p className="text-gray-600">You don't have permission to access this admin panel.</p>
+          <p className="text-gray-600">You don&apos;t have permission to access this admin panel.</p>
         </div>
       </div>
     );
